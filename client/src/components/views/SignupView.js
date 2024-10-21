797c2c1b7060aababb867ce8ac80d35f8c8711a1
himanshuarya3110 <himanshuarya3110@gmail.com>
2024-10-21 16:28:28 +0530

    Some Modifications

--- client/src/components/views/SignupView.js
+++ client/src/components/views/SignupView.js
@@ -76,11 +76,7 @@ const SignupView = () => {
       <Stack alignItems="center">
         <Typography variant="h2" color="text.secondary" sx={{ mb: 6 }}>
           <Link to="/" color="inherit" underline="none">
-<<<<<<< HEAD
             Chatters
-=======
-            PostIt
->>>>>>> 2fcbb2b778cdf8a1d18bf5b0c9923b98af9e2027
           </Link>
         </Typography>
         <Typography variant="h5" gutterBottom>
